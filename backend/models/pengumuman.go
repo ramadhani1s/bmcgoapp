040c07a388526f68f51c3da1aafd29a48c6ad96c
ramadhani1s <ramadhanisihotang4@gmail.com>
2026-06-09 23:43:42 +0700

    dikit

--- backend/models/pengumuman.go
+++ backend/models/pengumuman.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Pengumuman struct {
-	ID        int       `json:"id"`
-	AdminID   int       `json:"admin_id"`
-	Judul     string    `json:"judul"`
-	Isi       string    `json:"isi"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        int        `json:"id"`
+	AdminID   *int       `json:"admin_id"`
+	Judul     *string    `json:"judul"`
+	Isi       *string    `json:"isi"`
+	CreatedAt *time.Time `json:"created_at"`
 }
 
 type CreatePengumumanRequest struct {
